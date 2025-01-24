e0def2b824bda6f5b2654ed3bc731b055a10af3c
Cesar Carriel Sandroni <jccarriel@minsait.com>
2025-01-24 09:27:46 -0300

    feat:start http server

--- go.mod
+++ go.mod
@@ -2,4 +2,4 @@ module github.com/sandronister/webcrawler
 
 go 1.23.4
 
-
+require github.com/sandronister/go_broker v1.4.3
---
+++ internal/usecase/page_usecase.go
@@ -0,0 +1,16 @@
+package usecase
+
+import "github.com/sandronister/go_broker/pkg/broker/types"
+
+type PageUsecase struct {
+	broker types.IBroker
+}
+
+func NewPageUsecase(broker types.IBroker) *PageUsecase {
+	return &PageUsecase{
+		broker: broker,
+	}
+}
+
+func (u *PageUsecase) GetPage(url string) error {
+}
